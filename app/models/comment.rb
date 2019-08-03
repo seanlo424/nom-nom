9cef7d626cab40437b01e61c6c8f6a40794332d3
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-08-03 22:13:33 +0000

    Made rating dropdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
 	belongs_to :user
 	belongs_to :place
+
+	RATINGS = {
+		'one star': '1 star',
+		'two stars': '2 stars',
+		'three stars': '3 stars',
+		'four stars': '4 stars',
+		'five stars': '5 stars'
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
 end
