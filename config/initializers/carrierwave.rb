583af2b351f4ae580a1c3ce4f50d91715a4dea7f
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-08-04 19:24:16 +0000

    changed back AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
   config.fog_public = false
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AKIARZGOPLMF5ZSWDFMJ"],        # required
-    aws_secret_access_key: ENV["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"],        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["doogsbucket"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
