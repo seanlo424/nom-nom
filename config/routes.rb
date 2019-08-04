766316dc216232992c6c0f6b298a73ce61af74b9
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-08-04 17:14:24 +0000

    Working on adding image

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	root 'places#index'
 	resources :places do
 		resources :comments, only: :create
+		resources :photos
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
 end
